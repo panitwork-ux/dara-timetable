ba79004acbccc7ec9f9417d843d25d048613f887
panitwork-ux <panit.work@web1.dara.ac.th>
2026-04-03 14:23:18 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/dara-timetable/',   // ← เพิ่มบรรทัดนี้
   plugins: [react()],
   server: {
     host: '0.0.0.0',
