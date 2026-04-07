86958e6c5737fefe1a2857ef06a2205b564e1a1a
panitwork-ux <panit.work@web1.dara.ac.th>
2026-04-07 14:56:19 +0700

    config: vite external firebase esm

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dara-timetable/',   // ← เพิ่มบรรทัดนี้
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
+  base: '/dara-timetable/',
+  build: {
+    rollupOptions: {
+      external: [
+        /^https:\/\/www\.gstatic\.com\/firebasejs\/.*/
+      ]
+    }
   }
 })
